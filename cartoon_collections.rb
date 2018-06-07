c2966aed08ec2111cea3c731c7fd4a4c7da6bf81
Jason Pitts <juniorjr083@gmail.com>
2018-06-07 02:28:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(arr)# code an argument here
   # Your code here
-  arr.collect{|e| e.capitalise << "!"}
+  arr.collect{|e| e.capitalize << "!"}
 end
 
 def long_planeteer_calls# code an argument here
