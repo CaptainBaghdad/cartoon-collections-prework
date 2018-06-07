e2bdf5cac277b0ca95daf677ead8e4cfdb96b70a
Jason Pitts <juniorjr083@gmail.com>
2018-06-07 02:36:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  
+  arr & cheese_types.length >=1 ? true : nil
 end
